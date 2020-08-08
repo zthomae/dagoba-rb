7ef3142d030bc47dc107e057a2fbdc60bc1cd623
Zach Thomae <zach@thomae.co>
2020-08-08 16:03:32 -0500

    Add basic querying

--- dagoba.rb
+++ dagoba.rb
@@ -20,13 +20,13 @@
 # When you use the name of the label as a method, you get the edges originating
 # from the node you started with.
 #
-#   graph.find("bob").knows  # returns the node for "alice"
+#   graph.find("bob").knows.run  # returns the node for "alice"
 #
 # To go in the reverse order, you can either use inverse association names or
 # the reverse method:
 #
-#   graph.find("bob").known_by  # returns the node for "charlie"
-#   graph.find("bob").is_parent_of  # returns the node for "delta"
+#   graph.find("bob").known_by.run  # returns the node for "charlie"
+#   graph.find("bob").is_parent_of.run  # returns the node for "delta"
 #
 # If you grab multiple edges, the chained methods work in exactly the same
 # way. You can define aliases to make queries look cleaner. Alias names
@@ -35,10 +35,10 @@
 #   graph.relationship_type(:parents).means(:parent)
 #   graph.relationship_type(:children).reverses(:parents)
 #
-#   graph.parents  # returns the nodes for "bob" and "alice"
-#   graph.children  # returns the nodes for "charlie" and "bob"
+#   graph.parents.run  # returns the nodes for "bob" and "alice"
+#   graph.children.run  # returns the nodes for "charlie" and "bob"
 class Dagoba
-  Vertex = Struct.new(:id, :attributes, :out, :in, keyword_init: true)
+  Vertex = Struct.new(:id, :attributes, :relations, keyword_init: true)
   Edge = Struct.new(:relationship_type, :start_vertex, :end_vertex, keyword_init: true)
 
   class EstablishCommand
@@ -78,6 +78,37 @@ class Dagoba
     end
   end
 
+  class FindCommand
+    def initialize(graph:, start_vertex_id:)
+      @graph = graph
+      @relationship_types = graph.instance_variable_get(:@relationship_types)
+      @start_vertex = graph.send(:find_vertex, start_vertex_id)
+      @state = [@start_vertex]
+    end
+
+    def run
+      # TODO: Return value?
+      @state
+    end
+
+    private
+
+    def method_missing(symbol, *args)
+      if @relationship_types.has_key?(symbol)
+        @state = @state.flat_map { |vertex|
+          vertex.relations.select { |r| r.relationship_type == symbol }.map { |edge| edge.end_vertex }
+        }
+        self
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      @relationship_types.has_key?(symbol) || super
+    end
+  end
+
   def initialize
     @edges = []
     @vertices = []
@@ -90,26 +121,34 @@ class Dagoba
       raise "A vertex with id #{id} already exists"
     end
 
-    vertex = Vertex.new(id: id, attributes: attributes, out: [], in: [])
+    vertex = Vertex.new(id: id, attributes: attributes, relations: [])
     @vertices << vertex
     @vertex_index[id] = vertex
   end
 
-  def relationship(relationship_type, inverse: nil)
+  def relationship(relationship_type, inverse:)
+    unless inverse
+      raise ArgumentError.new("Must provide inverse relationship type")
+    end
     if @relationship_types.has_key?(relationship_type)
       raise "A relationship type with the name #{relationship_type} already exists"
-    elsif inverse && @relationship_types.has_key?(inverse)
+    elsif @relationship_types.has_key?(inverse)
       raise "A relationship type with the name #{inverse} already exists"
     end
 
     @relationship_types[relationship_type] = inverse
-    if inverse
-      @relationship_types[inverse] = relationship_type
-    end
+    @relationship_types[inverse] = relationship_type
   end
 
   def establish(start_vertex_id)
     EstablishCommand.new(
+      graph: self,
+      start_vertex_id: start_vertex_id
+    )
+  end
+
+  def find(start_vertex_id)
+    FindCommand.new(
       graph: self,
       start_vertex_id: start_vertex_id
     )
@@ -123,18 +162,17 @@ class Dagoba
 
   def create_edge(edge)
     @edges << edge
-    edge.start_vertex.out << edge
-    edge.end_vertex.in << edge
+    edge.start_vertex.relations << edge
 
-    if @relationship_types[edge.relationship_type]
+    inverse_type = @relationship_types[edge.relationship_type]
+    if inverse_type != edge.relationship_type || edge.start_vertex != edge.end_vertex
       inverse_edge = Edge.new(
-        relationship_type: @relationship_types[edge.relationship_type],
+        relationship_type: inverse_type,
         start_vertex: edge.end_vertex,
         end_vertex: edge.start_vertex
       )
       @edges << inverse_edge
-      inverse_edge.start_vertex.out << inverse_edge
-      inverse_edge.end_vertex.in << inverse_edge
+      edge.end_vertex.relations << inverse_edge
     end
   end
 end
--- dagoba_spec.rb
+++ dagoba_spec.rb
@@ -4,9 +4,15 @@ require_relative "./dagoba"
 
 describe Dagoba do
   describe "adding relationships" do
+    it "will not allow you to establish a relationship with no inverse" do
+      graph = Dagoba.new
+      expect { graph.relationship(:knows) }.to raise_error(ArgumentError)
+      expect { graph.relationship(:knows, inverse: nil) }.to raise_error(ArgumentError)
+    end
+
     it "will not allow you to establish a relationship with an invalid starting vertex" do
       graph = Dagoba.new
-      graph.relationship(:knows)
+      graph.relationship(:knows, inverse: :knows)
       graph.vertex("end")
       expect { graph.establish("start").knows("end") }.to raise_error(
         "Cannot establish relationship from nonexistent vertex start"
@@ -24,7 +30,7 @@ describe Dagoba do
       graph = Dagoba.new
       graph.vertex("start")
       graph.vertex("end")
-      graph.relationship(:knows)
+      graph.relationship(:knows, inverse: :knows)
       expect { graph.establish("start").knows("end") }.not_to raise_error
     end
 
@@ -39,7 +45,7 @@ describe Dagoba do
     it "will allow you to establish self-relationships" do
       graph = Dagoba.new
       graph.vertex("start")
-      graph.relationship(:knows)
+      graph.relationship(:knows, inverse: :knows)
       expect { graph.establish("start").knows("start") }.not_to raise_error
     end
 
@@ -47,7 +53,7 @@ describe Dagoba do
       graph = Dagoba.new
       graph.vertex("start")
       graph.vertex("end")
-      graph.relationship(:knows)
+      graph.relationship(:knows, inverse: :knows)
       expect { graph.establish("start").knows("end") }.not_to raise_error
       expect { graph.establish("start").knows("end") }.not_to raise_error
     end
@@ -56,21 +62,51 @@ describe Dagoba do
       graph = Dagoba.new
       graph.vertex("start")
       graph.vertex("end")
-      graph.relationship(:knows)
-      graph.relationship(:is_parent_of)
+      graph.relationship(:knows, inverse: :knows)
+      graph.relationship(:is_parent_of, inverse: :is_child_of)
       expect { graph.establish("start").knows("end") }.not_to raise_error
       expect { graph.establish("start").is_parent_of("end") }.not_to raise_error
     end
 
     it "will not allow you to declare duplicate relationship types" do
       graph = Dagoba.new
-      graph.relationship(:knows)
-      expect { graph.relationship(:knows) }.to raise_error(
+      graph.relationship(:knows, inverse: :knows)
+      expect { graph.relationship(:knows, inverse: :knows) }.to raise_error(
         "A relationship type with the name knows already exists"
       )
       expect { graph.relationship(:known_by, inverse: :knows) }.to raise_error(
         "A relationship type with the name knows already exists"
       )
+    end
+  end
+
+  describe "querying relationships" do
+    it "returns empty when a node has no relations of a given type" do
+      graph = Dagoba.new
+      graph.relationship(:knows, inverse: :knows)
+      graph.vertex("start")
+      expect(graph.find("start").knows.run).to be_empty
+    end
+
+    it "returns the correct number of results when a node has relations of a given type" do
+      graph = Dagoba.new
+      graph.relationship(:knows, inverse: :knows)
+      graph.vertex("start")
+      graph.vertex("end")
+      graph.establish("start").knows("start")
+      graph.establish("start").knows("end")
+      expect(graph.find("start").knows.run.length).to eq(2)
+    end
+
+    it "allows chaining queries" do
+      graph = Dagoba.new
+      graph.relationship(:parent_of, inverse: :child_of)
+      graph.vertex("alice")
+      graph.vertex("bob")
+      graph.vertex("charlie")
+      graph.establish("alice").parent_of("bob")
+      graph.establish("charlie").parent_of("bob")
+      expect(graph.find("alice").parent_of.child_of.run.length).to eq(2)
     end
   end
 end
